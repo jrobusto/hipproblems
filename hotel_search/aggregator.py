df796720d9cbf0ee010a9477957df960666c2031
Joshua Robusto <joshuarobusto@gmail.com>
2017-06-12 05:11:01 -0400

    Add handler modeled on existing request handler

--- hotel_search/aggregator.py
+++ hotel_search/aggregator.py
@@ -1,29 +1,51 @@
 import grequests
 import requests
 import simplejson as json
-from tornado import gen, ioloop, web
+from tornado import ioloop, web
 
 
-def pick_lowest_ecstasy(lists):
-    ecstasies = [l[-1]['ecstasy'] for l in lists]
-    lowest = min(ecstasies)
-    return ecstasies.index(lowest)
+class AggregatorApiHandler(web.RequestHandler):
+    def initialize(self):
+        self.providers = ['expedia', 'orbitz', 'priceline', 'travelocity', 'hilton']
+        self.urls = ['http://localhost:9000/scrapers/' + p for p in self.providers]
 
-providers = ['expedia', 'orbitz', 'priceline', 'travelocity', 'hilton']
-urls = ['http://localhost:9000/scrapers/' + p for p in providers]
+    @staticmethod
+    def pick_lowest_ecstasy(lists):
+        ecstasies = [l[-1]['ecstasy'] for l in lists]
+        lowest = min(ecstasies)
+        return ecstasies.index(lowest)
 
-rs = (grequests.get(u) for u in urls)
+    def get(self):
+        rs = (grequests.get(u) for u in self.urls)
+        responses = grequests.map(rs)
+        all_results = [res.json()['results'] for res in responses]
+        final = []
+        while all_results:
+            next_index = AggregatorApiHandler.pick_lowest_ecstasy(all_results)
+            final.append(all_results[next_index].pop())
+            if not all_results[next_index]:
+                del all_results[next_index]
+        final.reverse()
+        self.write({
+            "results": final,
+        })
 
-responses = grequests.map(rs)#[requests.get('http://localhost:9000/scrapers/' + p) for p in providers]
-
-all_results = [res.json()['results'] for res in responses]
-
-final = []
-while all_results:
-    next_index = pick_lowest_ecstasy(all_results)
-    final.append(all_results[next_index].pop())
-    if not all_results[next_index]:
-        del all_results[next_index]
+ROUTES = [
+    (r"/hotels/search", AggregatorApiHandler),
+]
 
 
-print final
+def run():
+    app = web.Application(
+        ROUTES,
+        debug=True,
+    )
+
+    app.listen(8000)
+    print "Server (re)started. Listening on port 8000"
+
+    ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    run()
